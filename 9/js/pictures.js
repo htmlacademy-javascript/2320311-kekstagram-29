9e51e3bbe41a25b67bf7de56552a22e3fe7fe293
Keks <keks@htmlacademy.ru>
2023-07-14 13:11:22 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/pictures.js
@@ -0,0 +1,19 @@
+import { randomDescription } from './data.js';
+
+const picturesContainer = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const otherPictures = randomDescription();
+
+const otherPicturesFragment = document.createDocumentFragment();
+
+otherPictures.forEach(({ url, description, likes, comments }) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__img').alt = description;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  otherPicturesFragment.appendChild(pictureElement);
+});
+
+picturesContainer.appendChild(otherPicturesFragment);
