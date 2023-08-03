de777932aefa3f312d5e4399d2908b471fc80230
Keks <keks@htmlacademy.ru>
2023-08-03 09:29:42 +0300

    :heavy_check_mark: Сборка #14

---
+++ 14/js/pictures.js
@@ -0,0 +1,81 @@
+import { randomDescription } from './data.js';
+import {setImgProps} from './functions.js';
+
+const picturesContainer = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const otherPictures = randomDescription();
+
+const otherPicturesFragment = document.createDocumentFragment();
+
+otherPictures.forEach(({ url, description, likes, comments }) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__img').alt = description;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  otherPicturesFragment.appendChild(pictureElement);
+});
+
+picturesContainer.appendChild(otherPicturesFragment);
+
+const pictureContainer = document.querySelector('.pictures');
+/*
+ * Функция на основе шаблона #picture создает и возвращает DOM-элемент, соответствующей фотографии
+ */
+const createPictureElement = () => {
+  const pictureTemplate = document.querySelector('#picture')
+    .content.querySelector('.picture');
+  return pictureTemplate.cloneNode(true);
+};
+/*
+ * Функция устанавливает значения свойст для созданной фотографии pictureElement
+ */
+const processPicture = (pictureElement, {url, description, likes, comments}) => {
+  // Свойства самого изображения (источник и alt-текст)
+  setImgProps(pictureElement.querySelector('.picture__img'), url, description);
+  // Количество лайков
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  // Количество комментариев
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+};
+/*
+ * Функция на основе данных создает DOM-элемент фотографии.
+ */
+const createPicture = (photo, showBigPicture) => {
+  const pictureElement = createPictureElement();
+  processPicture(pictureElement, photo);
+  pictureElement.addEventListener('click', () => {
+    showBigPicture(photo);
+  });
+  return pictureElement;
+};
+/*
+ * Удаляет опубликованные ранее картинки
+ * Нужна для того, чтобы не замножать картинки при изменении фильтра
+ */
+const removePictures = () => {
+  // Ищем все отрисованные ранее картинки
+  const pictures = pictureContainer.querySelectorAll('.picture');
+  // и удаляем их
+  pictures.forEach((picture) => picture.remove());
+};
+
+/*
+ * Функция отображает фотографии на странице.
+ * Через параметр photos передается массив данных о фотографиях,
+ * параметр showBigPicture - ссылка на функцию, вызываемую при клике на миниатюре.
+ */
+const showPictures = (photos, showBigPicture) => {
+  const pictureFragment = document.createDocumentFragment();
+  photos.forEach((photo) => {
+    pictureFragment.append(createPicture(photo, showBigPicture));
+  });
+  // Удаление опубликованных ранее картинок
+  removePictures();
+  // Публикация новых
+  pictureContainer.appendChild(pictureFragment);
+};
+export {showPictures};
+
+//Просто da
